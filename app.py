6ef6ea8a64be225af85725bb38fc038ce91b3f3b
sathyainfotech <84429523+sathyainfotech@users.noreply.github.com>
2021-06-10 08:14:18 -0700

    File Upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('Pagination.html')
+
+@app.route('/page1')
+def page1():
+    return render_template("Pagination.html")
+
+@app.route('/page2')
+def page2():
+    return render_template("Page_2.html")
+
+@app.route('/page3')
+def page3():
+    return render_template("Page_3.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
